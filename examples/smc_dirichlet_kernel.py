a53d786f06ebaa614a1b5e9697793509601734a9
huypn12 <huy.phung1292@gmail.com>
2021-01-11 19:54:51 +0100

    WIP, add simple Beta perturbation

--- examples/smc_dirichlet_kernel.py
+++ examples/smc_dirichlet_kernel.py
@@ -2,13 +2,29 @@ import numpy as np
 from typing import List
 
 
+class LogitNormal:
+    pass
+
+
+class MulivariateLogitNormal:
+    pass
+
+
 class ExampleSmcKernel(object):
     def __init__(self) -> None:
         super().__init__()
         self.rng: np.random.Generator = np.random.default_rng()
-        self.particle_count: int = 100
+        self.particle_count: int = 1000
 
-    def gen_smc_univariate(self) -> List[float]:
+    def gen_smc_univariate_beta(self) -> List[float]:
+        trace = np.zeros(self.particle_count, dtype=float)
+        trace[0] = self.rng.uniform(0, 1)
+        for i in range(1, self.particle_count):
+            alpha = beta = 1 / trace[i - 1]
+            trace[i] = self.rng.beta(alpha, beta)
+        return trace
+
+    def gen_smc_univariate_logitnormal(self) -> List[float]:
         trace = np.zeros(self.particle_count, dtype=float)
         trace[0] = self.rng.uniform(0, 1)
         for i in range(1, self.particle_count):
@@ -21,7 +37,6 @@ class ExampleSmcKernel(object):
         trace[0] = self.rng.uniform(0, 1, size=pdim)
         for i in range(1, self.particle_count):
             alpha = [(1 / p) for p in trace[i - 1]]
-            print(alpha)
             trace[i] = self.rng.dirichlet(alpha)
         return trace
 
@@ -33,10 +48,9 @@ def main():
     for i in range(0, len(trace)):
         print(trace[i])
     print("Dirichlet kernel smc")
-    trace = example.gen_smc_multivariate(4)
+    trace = example.gen_smc_multivariate(2)
     for i in range(0, len(trace)):
-        pass
-    # print(trace[i])
+        print(trace[i])
 
 
 if __name__ == "__main__":
