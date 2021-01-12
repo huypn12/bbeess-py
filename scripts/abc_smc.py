33d208bed47052fda893b5208ce227a6adda1d3c
huypn12 <huy.phung1292@gmail.com>
2021-01-12 19:50:15 +0100

    fucking progress

--- scripts/abc_smc.py
+++ scripts/abc_smc.py
@@ -1,12 +1,23 @@
+import numpy as np
+import scipy as sp
+
+
 class AbcSmc(object):
     def __init__(self) -> None:
         super().__init__()
 
-    @staticmethod
-    def _distance(v1: np.array, v2: np.array) -> float:
+    def _distance(self, v1: np.array, v2: np.array) -> float:
         """Distance between two vector"""
         return np.linalg.norm(v1 - v2)
 
-    @staticmethod
     def _kernel(self, s1, s2, threshold):
-        return 1 if (SmcRf._distance(s1, s2) > threshold) else 0
+        return 1 if (AbcSmc._distance(s1, s2) > threshold) else 0
+
+    def _check_model_smc(self):
+        pass
+
+    def _estimate_obs_llh(self):
+        pass
+
+    def _estimate_check_llh(self):
+        pass
