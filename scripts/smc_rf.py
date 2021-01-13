00d4d8c72a720639b89fb97e51081c64c2e39ab1
huypn12 <huy.phung1292@gmail.com>
2021-01-13 18:09:50 +0100

    fucking progress; smc scheme

--- scripts/smc_rf.py
+++ scripts/smc_rf.py
@@ -7,63 +7,82 @@ import numpy as np
 import scipy as sp
 
 
+class MyPrismProgram(object):
+    def __init__(
+        self,
+        prism_model_file: str,
+        prism_props_file: str,
+    ) -> None:
+        super().__init__()
+        self.prism_model_file: str = prism_model_file
+        self.prism_props_file: str = prism_props_file
+        self.prism_program = stormpy.parse_prism_program(self.prism_model_file)
+        props_str = self._load_props_file()
+        self.prism_props = stormpy.parse_properties_for_prism_program(
+            props_str, self.prism_program
+        )
+
+    def _load_props_file(self):
+        lines = []
+        with open(self.prism_props_file, "r") as fptr:
+            lines = fptr.readlines()
+        return ";".join(lines)
+
+
 class SmcRf(object):
     """In case of likelihood can be cheaply calculated (closed form solution of the property is known)"""
 
     def __init__(
         self,
-        prism_model_file_path: str,
-        verify_property: str,
-        observe_properties: List[str],
+        prism_model_file: str,
+        prism_props_file: str,
         particle_count: int,
-        pertubation_count: int,
-        verify_threshold: float,
+        perturbation_count: int,
+        check_threshold: float,
     ) -> None:
         super().__init__()
-        # PRISM model and one PCTL property
-        self.prism_program = stormpy.parse_prism_program(prism_model_file_path)
-        props_str = ";".join([verify_property] + observe_properties)
-        props = stormpy.parse_properties_for_prism_program(
-            props_str,
-            self.prism_program,
+        self.rng: np.random.Generator = np.random.default_rng()
+        # PRISM model configuration
+        self.my_prism_program = MyPrismProgram(prism_model_file, prism_props_file)
+        self.model = stormpy.build_parametric_model(
+            self.my_prism_program.prism_program,
+            self.my_prism_program.prism_props,
         )
-        self.model = stormpy.build_parametric_model(self.prism_program, props)
         self.model_parameters = self.model.collect_probability_parameters()
-        self.verify_property = props[0]
-        self.verify_rf = stormpy.model_checking(self.model, self.verify_property).at(
+        self.check_prop = self.my_prism_program.prism_props[0]
+        self.check_rf = stormpy.model_checking(self.model, self.check_prop).at(
             self.model.initial_states[0]
         )
-        self.obs_props = props[1:]
-        self.obs_props_rf = [
+        self.obs_props = self.my_prism_program.prism_props[1:]
+        self.obs_rf = [
             stormpy.model_checking(self.model, obs_prop).at(
                 self.model.initial_states[0]
             )
-            for obs_prop in self.observe_properties
+            for obs_prop in self.obs_props
         ]
-        self.current_param_values = np.array(len(self.model_parameters), dtype=np.float)
         self.instantiator = stormpy.pars.PDtmcInstantiator(self.model)
         self.instantiated_model = None
 
         # SMC configuration
         self.param_space_sample: List = []
         self.particle_count: int = particle_count
-        self.pertubation_count: int = pertubation_count
-        self.verify_threshold: int = verify_threshold
+        self.perturbation_count: int = perturbation_count
+        self.check_threshold: int = check_threshold
+        self.current_param = np.zeros(len(self.model_parameters), dtype=np.float)
 
-    def _instantiate_pmodel(self, params: np.array):
+    def _instantiate_pmodel(self, param: np.array):
         point = dict()
         for i, p in enumerate(self.model_parameters):
-            point[p] = params[i]
+            point[p] = stormpy.RationalRF(param[i])
         self.instantiated_model = self.instantiator.instantiate(point)
 
     def _init(self):
-        sample_size = len(self.current_param_values)
-        sampled_params = np.random.uniform(0, 1, sample_size)
-        self.current_param_values = sampled_params
-        self._instantiate_pmodel(sampled_params)
+        sample_size = len(self.current_param)
+        sampled_param = np.random.uniform(0, 1, sample_size)
+        self.current_param = sampled_param
+        self._instantiate_pmodel(sampled_param)
 
     def _perturbate(self, param) -> np.array:
-        """Draw new parameter from Normal distribution"""
         # TODO: properly designed perturbation function; proof of convergence/KL distance
         for i, p_i in enumerate(param):
             alpha = beta = 1 / p_i
@@ -74,32 +93,32 @@ class SmcRf(object):
         # Accepted point: points which satisfy the properties
         # Weight: likelihood to generate the observed data
         for m in range(0, self.particle_count):
-            for _ in range(0, self.pertubation_count):
-                candidate_params: np.array = None
+            for _ in range(0, self.perturbation_count):
+                candidate_param: np.array = None
                 if m == 0:
-                    param_dim = len(self.current_param_values)
-                    candidate_params = np.random.uniform(0, 1, param_dim)
+                    param_dim = len(self.current_param)
+                    candidate_param = np.random.uniform(0, 1, param_dim)
                 else:
-                    candidate_params = self._perturbate(self.current_param_values)
-                if not self._is_candidate_params_valid(candidate_params):
+                    candidate_param = self._perturbate(self.current_param)
+                if not self._is_candidate_params_valid(candidate_param):
                     continue
-                llh_obs = self._estimate_obs_llh(candidate_params)
-                llh_prop = self._estimate_prop_llh(candidate_params)
-                if llh_prop < self.threshold:
+                llh_obs = self._estimate_obs_llh(candidate_param)
+                llh_prop = self._estimate_check_llh(candidate_param)
+                if llh_prop < self.check_threshold:
                     continue
-                self.trace.append({candidate_params: llh_obs})
+                self.param_space_sample.append((candidate_param, llh_obs))
 
-    def _estimate_obs_llh(self, params: np.array):
+    def _estimate_obs_llh(self, param: np.array):
         point = dict()
         for i, p in enumerate(self.model_parameters):
-            point[p] = params[i]
-        return [float(rf.evaluate(point)) for rf in self.obs_props_rf]
+            point[p] = stormpy.RationalRF(param[i])
+        return [float(rf.evaluate(point)) for rf in self.obs_rf]
 
-    def _estimate_sat_prob(self, params: np.array):
+    def _estimate_check_llh(self, param: np.array):
         point = dict()
         for i, p in enumerate(self.model_parameters):
-            point[p] = params[i]
-        return float(self.verify_rf.evaluate(point))
+            point[p] = stormpy.RationalRF(param[i])
+        return float(self.check_rf.evaluate(point))
 
     def _is_candidate_params_valid(self, p: np.array):
         for _p in p:
@@ -110,3 +129,25 @@ class SmcRf(object):
     def run(self):
         self._init()
         self._smc()
+        print(self.param_space_sample)
+
+
+def main():
+    prism_model_file = (
+        "/home/huypn12/Work/UniKonstanz/MCSS/bbeess-py/examples/data/zeroconf.pm"
+    )
+    prism_props_file = (
+        "/home/huypn12/Work/UniKonstanz/MCSS/bbeess-py/examples/data/zeroconf.pctl"
+    )
+    smc_rf = SmcRf(
+        prism_model_file=prism_model_file,
+        prism_props_file=prism_props_file,
+        particle_count=100,
+        perturbation_count=10,
+        check_threshold=0.01,
+    )
+    smc_rf.run()
+
+
+if __name__ == "__main__":
+    main()
