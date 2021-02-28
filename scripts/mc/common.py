7ab3b04a82c4716cfdad0fa8f2e30d33e2a2c457
huypn12 <huy.phung1292@gmail.com>
2021-02-28 19:59:29 +0100

    WIP

--- scripts/mc/common.py
+++ scripts/mc/common.py
@@ -8,3 +8,23 @@ class AbstractObservableModel(abc.ABC):
     @abc.abstractmethod
     def estimate_log_llh(self, particle: np.array) -> float:
         pass
+
+
+class AbstractRationalModel(abc.ABC):
+    @abc.abstractmethod
+    def estimate_log_llh(self, particle: np.array) -> float:
+        pass
+
+
+class AbstractSimulationModel(abc.ABC):
+    @abc.abstractmethod
+    def estimate_log_llh(self, theta: np.array) -> float:
+        pass
+
+    @abc.abstractmethod
+    def estimate_distance(
+        self,
+        theta: np.array,
+        s_sim: np.array,
+    ) -> float:
+        pass
