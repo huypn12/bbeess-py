65d8b847a4b0bff2c40ed60afed1e4cae7514c9d
huypn12 <huy.phung1292@gmail.com>
2021-03-01 17:35:52 +0100

    WIP

--- experiments/sir_rational.py
+++ experiments/sir_rational.py
@@ -6,3 +6,4 @@ from scripts.prism.prism_model import AbstractModelRational
 class SirRational(AbstractModelRational):
     def __init__(self, prism_model_file: str, prism_props_file: str) -> None:
         super().__init__(prism_model_file, prism_props_file)
+        
