7461b4414b76b9c12df97c5c340bb289729490e7
huypn12 <huy.phung1292@gmail.com>
2021-03-11 22:21:46 +0100

    update test

--- tests/test_storm_pars.py
+++ tests/test_storm_pars.py
@@ -22,37 +22,30 @@ class TestStormPars(unittest):
         path = stormpy.examples.files.prism_pdtmc_die
         prism_program = stormpy.parse_prism_program(path)
         formula_str = "P=? [F s=7 & d=2]"
-        properties = stormpy.parse_properties_for_prism_program(
+        self.properties = stormpy.parse_properties_for_prism_program(
             formula_str, prism_program
         )
-        model = stormpy.build_parametric_model(prism_program, properties)
+        self.model = stormpy.build_parametric_model(prism_program, self.properties)
+        parameters = self.model.collect_probability_parameters()
+        self.assertEqual(len(parameters), 2)
 
+    def test_rf_evaluation(self):
+        point = dict()
+        for x in self.model.collect_probability_parameters():
+            print(x.name)
+            point[x] = pycarl.cln.cln.Rational(0.4)
+        initial_state = self.model.initial_states[0]
+        result = stormpy.model_checking(self.model, self.properties[0])
+        rf = result.at(initial_state)
+        print("Rational function of the desired property {}".format(rf))
+        print(float(rf.evaluate(point)))
 
-def example_parametric_models_01():
-    # Check support for parameters
-    if not config.storm_with_pars:
-        print("Support parameters is missing. Try building storm-pars.")
-        return
-
-    print("Model initial states: {}".format(model.initial_states))
-    print("Model supports parameters: {}".format(model.supports_parameters))
-    parameters = model.collect_probability_parameters()
-    assert len(parameters) == 2
-    point = dict()
-    for x in parameters:
-        print(x.name)
-        point[x] = pycarl.cln.cln.Rational(0.4)
-    initial_state = model.initial_states[0]
-    result = stormpy.model_checking(model, properties[0])
-    rf = result.at(initial_state)
-    print("Rational function of the desired property {}".format(rf))
-    print(float(rf.evaluate(point)))
-
-    instantiator = stormpy.pars.PDtmcInstantiator(model)
-    instantiated_model = instantiator.instantiate(point)
-    result = stormpy.model_checking(instantiated_model, properties[0])
-    print(result.at(0))
-
-
-if __name__ == "__main__":
-    example_parametric_models_01()
+    def test_parametric_checking(self):
+        point = dict()
+        for x in self.model.collect_probability_parameters():
+            print(x.name)
+            point[x] = pycarl.cln.cln.Rational(0.4)
+        instantiator = stormpy.pars.PDtmcInstantiator(self.model)
+        instantiated_model = instantiator.instantiate(point)
+        result = stormpy.model_checking(instantiated_model, self.properties[0])
+        print(result)
