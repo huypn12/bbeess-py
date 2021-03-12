75466085f7a121b5d20bf44a59e0b63993bd847c
huypn12 <huy.phung1292@gmail.com>
2021-03-12 01:17:21 +0100

    move data files

--- experiments/data/model_generator/zeroconf_generator.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from typing import List, Dict, Any
-
-
-class ZeroconfGenerator(object):
-    def __init__(self, state_count: int) -> None:
-        self.adj_list: List[Any] = []
