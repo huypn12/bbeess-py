95b9db974578120666f5a8fccc32aaf8bcecfa4e
huypn12 <huy.phung1292@gmail.com>
2021-03-12 01:17:48 +0100

    move data files

---
+++ data/model_generator/sir_generator.py
@@ -0,0 +1,11 @@
+from typing import List, Type, Dict
+
+
+class SirGenerator(object):
+    def __init__(self, s0, i0, r0) -> None:
+        self.s0 = s0
+        self.i0 = i0
+        self.r0 = r0
+
+    def _compose(self):
+        pass
