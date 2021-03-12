8e05352e81cc6a469d695eeb1bb7bc58b9687571
huypn12 <huy.phung1292@gmail.com>
2021-03-12 01:16:31 +0100

    add test for bounded and unbounded properties

--- tests/test_mh_rf_uniform_kernel.py
+++ tests/test_mh_rf_uniform_kernel.py
@@ -4,4 +4,10 @@ from scripts.mc.mh_rf_uniform_kernel import MhRfUniformKernel
 
 
 class TestMhRfUniformKernel(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.prism_model_file = (
+            "/home/huypn12/Works/mcss/bbeess-py/experiments/data/sir_310.pm"
+        )
+        self.prism_props_file = (
+            "/home/huypn12/Works/mcss/bbeess-py/experiments/data/sir_310.pctl"
+        )
--- tests/test_storm_pars.py
+++ tests/test_storm_pars.py
@@ -21,9 +21,10 @@ class TestStormPars(unittest.TestCase):
 
         path = stormpy.examples.files.prism_pdtmc_die
         prism_program = stormpy.parse_prism_program(path)
-        formula_str = "P=? [F s=7 & d=2]"
+        unbounded_formula_str = "P=? [F s=7 & d=2]"
+        bounded_formula_str = "P>=0.7 [F s=7 & d=2]"
         self.properties = stormpy.parse_properties_for_prism_program(
-            formula_str, prism_program
+            ";".join([unbounded_formula_str, bounded_formula_str]), prism_program
         )
         self.model = stormpy.build_parametric_model(prism_program, self.properties)
         parameters = self.model.collect_probability_parameters()
@@ -32,7 +33,6 @@ class TestStormPars(unittest.TestCase):
     def test_rf_evaluation(self):
         point = dict()
         for x in self.model.collect_probability_parameters():
-            print(x.name)
             point[x] = pycarl.cln.cln.Rational(0.4)
         initial_state = self.model.initial_states[0]
         result = stormpy.model_checking(self.model, self.properties[0])
@@ -40,7 +40,7 @@ class TestStormPars(unittest.TestCase):
         self.assertEqual(str(rf), "((p)^2 * (q+(-1)))/(p*q+(-1))")
         self.assertEqual(float(rf.evaluate(point)), 0.1142857142857143)
 
-    def test_parametric_checking(self):
+    def test_unbounded_parametric_checking(self):
         point = dict()
         for x in self.model.collect_probability_parameters():
             point[x] = pycarl.cln.cln.Rational(0.4)
@@ -49,3 +49,13 @@ class TestStormPars(unittest.TestCase):
         result = stormpy.model_checking(instantiated_model, self.properties[0])
         initial_state = self.model.initial_states[0]
         self.assertEqual(result.at(initial_state), 0.11428571428571428)
+
+    def test_bounded_parametric_checking(self):
+        point = dict()
+        for x in self.model.collect_probability_parameters():
+            point[x] = pycarl.cln.cln.Rational(0.4)
+        instantiator = stormpy.pars.PDtmcInstantiator(self.model)
+        instantiated_model = instantiator.instantiate(point)
+        result = stormpy.model_checking(instantiated_model, self.properties[1])
+        initial_state = self.model.initial_states[0]
+        self.assertFalse(result.at(initial_state))
