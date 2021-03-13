640326143d30637b0eedf2aabc9ca8496c82ec64
huypn12 <huy.phung1292@gmail.com>
2021-03-13 17:44:54 +0100

    update unit test

--- tests/test_prism_smc_executor.py
+++ tests/test_prism_smc_executor.py
@@ -14,10 +14,16 @@ class TestPrismSmcSprtExecutor(unittest.TestCase):
         self.prism_sprt_executor = PrismSmcSprtExecutor(
             prism_model_file=self.prism_model_file,
             prism_props_file=self.prism_props_file,
-            simwidth=0.00005,
-            simconf=None,
+        )
+        self.prism_apmc_executor = PrismSmcSprtExecutor(
+            prism_model_file=self.prism_model_file,
+            prism_props_file=self.prism_props_file,
         )
 
     def test_sprt_sir310(self):
-        result = self.prism_sprt_executor.exec("pa=1,pr=2")
+        result = self.prism_sprt_executor.exec(model_consts_str="pa=1,pr=2")
+        self.assertFalse(result)
+
+    def test_apmc_sir310(self):
+        result = self.prism_apmc_executor.exec(model_consts_str="pa=1,pr=2")
         self.assertFalse(result)
