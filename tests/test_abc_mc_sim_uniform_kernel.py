abe3c39edfbcf168360b33627faf77d8591625aa
huypn12 <huy.phung1292@gmail.com>
2021-03-14 14:58:09 +0100

    unit test

---
+++ tests/test_abc_mc_sim_uniform_kernel.py
@@ -0,0 +1,34 @@
+import unittest
+
+import numpy as np
+
+from scripts.model.simple_prism_sim_model import SimpleSimModel
+from scripts.mc.abc_mc_smc_uniform_kernel import AbcMcSmcUniformKernel
+
+
+class TestAbcMcSimUniformKernel(unittest.TestCase):
+    def setUp(self):
+        self.prism_model_file = (
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_310.pm"
+        )
+        self.prism_props_file = (
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_310.pctl"
+        )
+        self.model = SimpleSimModel(
+            self.prism_model_file,
+            self.prism_props_file,
+            obs_labels=["bscc_300", "bscc_202", "bscc_103", "bscc_013"],
+        )
+        self.mc = AbcMcSmcUniformKernel(
+            model=self.model,
+            interval=[0, 0.01],
+            particle_dim=2,
+            particle_trace_len=100,
+            observed_data=[28, 12, 17, 43],
+        )
+
+    def test_exec(self):
+        self.mc.run()
+        particle, trace, weights = self.mc.get_result()
+        print(particle)
+        pass
