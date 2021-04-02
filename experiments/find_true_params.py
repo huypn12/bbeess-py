00ba53b38ec8c6cfa37c211a0f0ecf9b602064c3
huypn12 <huy.phung1292@gmail.com>
2021-04-02 23:02:58 +0200

    daily progress

--- experiments/find_true_params.py
+++ experiments/find_true_params.py
@@ -20,65 +20,86 @@ class FindTrueParams(object):
         prism_model_file: str,
         prism_props_file: str,
         interval: Tuple[float, float],
+        simulation_count: int,
     ):
         self.prism_model_file = prism_model_file
         self.prism_props_file = prism_props_file
+        logging.info(f"{str(datetime.now())} PRISM model: {prism_model_file}")
+        logging.info(f"{str(datetime.now())} PRISM props: {prism_props_file}")
         self.model = SimpleRfModel(
             self.prism_model_file,
             self.prism_props_file,
         )
         self.interval = interval
-        self.true_params: List[np.array] = []
+        self.simulation_count = simulation_count
 
     def search(self):
-        while len(self.true_params) < 10:
+        i: int = 0
+        while i < 10:
             alpha = np.random.uniform(*self.interval)
             beta = np.random.uniform(*self.interval)
             sat = self.model.check_bounded(np.array([alpha, beta]))
-            print(sat)
             if sat:
-                self.true_params.append(np.array([alpha, beta]))
                 logging.info(
                     f"{str(datetime.now())} True parameter (SAT): {[alpha, beta]}"
                 )
+                self.simulate(np.array([alpha, beta]))
+                i += 1
 
-    def simulate(self):
-        for p in self.true_params:
-            logging.info(f"{str(datetime.now())} Synthesizing data")
-            self.synthetic_data = self.model.simulate(p, 10000)
-            logging.info(f"{str(datetime.now())} Synthetic data: {self.synthetic_data}")
+    def simulate(self, p: np.array):
+        self.synthetic_data = self.model.simulate(p, self.simulation_count)
+        logging.info(f"{str(datetime.now())} Synthetic data: {self.synthetic_data}")
 
     def exec(self):
         self.search()
-        self.simulate()
 
 
 def main():
-    """
-    prism_model_file: str = "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_5_1_0.pm"
-    prism_props_file: str = (
-        "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_5_1_0.pctl"
-    )
-    experiment = FindTrueParams(prism_model_file, prism_props_file, (0, 0.1))
-    experiment.exec()
+    configs = [
+        (
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_3_1_0.pm",
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_3_1_0.pctl",
+            (0, 0.1),
+            10000,
+        ),
+        (
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_5_1_0.pm",
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_5_1_0.pctl",
+            (0, 0.1),
+            10000,
+        ),
+        (
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_5_1_0_a.pm",
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_5_1_0_a.pctl",
+            (0, 0.1),
+            200,
+        ),
+        (
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_10_1_0.pm",
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_10_1_0.pctl",
+            (0, 0.1),
+            10000,
+        ),
+        (
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_10_1_0_a.pm",
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_10_1_0_a.pctl",
+            (0, 0.1),
+            200,
+        ),
+        (
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_15_1_0.pm",
+            "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_15_1_0.pctl",
+            (0, 0.1),
+            10000,
+        ),
+    ]
 
-    prism_model_file: str = (
-        "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_10_1_0.pm"
-    )
-    prism_props_file: str = (
-        "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_10_1_0.pctl"
-    )
-    experiment = FindTrueParams(prism_model_file, prism_props_file, (0, 0.1))
-    experiment.exec()
-    """
-    prism_model_file: str = (
-        "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_15_1_0.pm"
-    )
-    prism_props_file: str = (
-        "/home/huypn12/Works/mcss/bbeess-py/data/prism/sir_15_1_0.pctl"
-    )
-    experiment = FindTrueParams(prism_model_file, prism_props_file, (0, 0.1))
-    experiment.exec()
+    for cfg in configs:
+        prism_model_file, prism_props_file, interval, simulation_count = cfg
+        experiment = FindTrueParams(
+            prism_model_file, prism_props_file, interval, simulation_count
+        )
+        experiment.exec()
 
 
 if __name__ == "__main__":
