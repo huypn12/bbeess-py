90fbce484e55d1a77420447b56dc5eb979822d86
huypn12 <huy.phung1292@gmail.com>
2021-04-02 18:32:23 +0200

    fix model issue

--- data/prism/sir_5_1_0_a.pm
+++ data/prism/sir_5_1_0_a.pm
@@ -1,11 +1,13 @@
 dtmc
   const double alpha;
   const double beta;
-module sir_5_1_0
+
+module sir_5_1_0 
+	s : [0..6] init 5;
+	i : [0..6] init 1;
+	r : [0..6] init 0;
  
-	s : [0..5] init 5;
-	i : [0..5] init 1;
-	r : [0..5] init 0; [] s=5 & i=1 & r=0 -> ((5*alpha)/(5*alpha+6*beta)):(s'=4) & (i'=2) & (r'=0) + ((1*beta)/(5*alpha+6*beta)):(s'=5) & (i'=0) & (r'=1) + ((5*beta)/(5*alpha+6*beta)):(s'=5) & (i'=1) & (r'=0);
+	[] s=5 & i=1 & r=0 -> ((5*alpha)/(5*alpha+6*beta)):(s'=4) & (i'=2) & (r'=0) + ((1*beta)/(5*alpha+6*beta)):(s'=5) & (i'=0) & (r'=1) + ((5*beta)/(5*alpha+6*beta)):(s'=5) & (i'=1) & (r'=0);
 	[] s=4 & i=2 & r=0 -> ((4*alpha)/(5*alpha+6*beta)):(s'=3) & (i'=3) & (r'=0) + ((2*beta)/(5*alpha+6*beta)):(s'=4) & (i'=1) & (r'=1) + ((1*alpha+4*beta)/(5*alpha+6*beta)):(s'=4) & (i'=2) & (r'=0);
 	[] s=3 & i=3 & r=0 -> ((3*alpha)/(5*alpha+6*beta)):(s'=2) & (i'=4) & (r'=0) + ((3*beta)/(5*alpha+6*beta)):(s'=3) & (i'=2) & (r'=1) + ((2*alpha+3*beta)/(5*alpha+6*beta)):(s'=3) & (i'=3) & (r'=0);
 	[] s=2 & i=4 & r=0 -> ((2*alpha)/(5*alpha+6*beta)):(s'=1) & (i'=5) & (r'=0) + ((4*beta)/(5*alpha+6*beta)):(s'=2) & (i'=3) & (r'=1) + ((3*alpha+2*beta)/(5*alpha+6*beta)):(s'=2) & (i'=4) & (r'=0);
@@ -25,8 +27,17 @@ module sir_5_1_0
 	[] s=2 & i=1 & r=3 -> ((2*alpha)/(5*alpha+6*beta)):(s'=1) & (i'=2) & (r'=3) + ((1*beta)/(5*alpha+6*beta)):(s'=2) & (i'=0) & (r'=4) + ((3*alpha+5*beta)/(5*alpha+6*beta)):(s'=2) & (i'=1) & (r'=3);
 	[] s=3 & i=2 & r=1 -> ((3*alpha)/(5*alpha+6*beta)):(s'=2) & (i'=3) & (r'=1) + ((2*beta)/(5*alpha+6*beta)):(s'=3) & (i'=1) & (r'=2) + ((2*alpha+4*beta)/(5*alpha+6*beta)):(s'=3) & (i'=2) & (r'=1);
 	[] s=3 & i=1 & r=2 -> ((3*alpha)/(5*alpha+6*beta)):(s'=2) & (i'=2) & (r'=2) + ((1*beta)/(5*alpha+6*beta)):(s'=3) & (i'=0) & (r'=3) + ((2*alpha+5*beta)/(5*alpha+6*beta)):(s'=3) & (i'=1) & (r'=2);
-	[] s=4 & i=1 & r=1 -> ((4*alpha)/(5*alpha+6*beta)):(s'=3) & (i'=2) & (r'=1) + ((1*beta)/(5*alpha+6*beta)):(s'=4) & (i'=0) & (r'=2) + ((1*alpha+5*beta)/(5*alpha+6*beta)):(s'=4) & (i'=1) & (r'=1); 
+	[] s=4 & i=1 & r=1 -> ((4*alpha)/(5*alpha+6*beta)):(s'=3) & (i'=2) & (r'=1) + ((1*beta)/(5*alpha+6*beta)):(s'=4) & (i'=0) & (r'=2) + ((1*alpha+5*beta)/(5*alpha+6*beta)):(s'=4) & (i'=1) & (r'=1);
+	[] s=0 & i=0 & r=6 -> (s'=0) & (i'=0) & (r'=6);
+	[] s=1 & i=0 & r=5 -> (s'=1) & (i'=0) & (r'=5);
+	[] s=2 & i=0 & r=4 -> (s'=2) & (i'=0) & (r'=4);
+	[] s=3 & i=0 & r=3 -> (s'=3) & (i'=0) & (r'=3);
+	[] s=4 & i=0 & r=2 -> (s'=4) & (i'=0) & (r'=2);
+	[] s=5 & i=0 & r=1 -> (s'=5) & (i'=0) & (r'=1); 
 endmodule
-label "bscc_k_0_ge2" = s=0 & i=0 & r=6 | s=1 & i=0 & r=5 | s=2 & i=0 & r=4 | s=3 & i=0 & r=3 ;
-label "bscc_4_0_2"   = s=4 & i=0 & r=2 ;
-label "bscc_5_0_1"   = s=5 & i=0 & r=1 ;
+
+label "bscc_k_0_geq4" = s=0 & i=0 & r=6 | s=1 & i=0 & r=5 | s=2 & i=0 & r=4 ;
+label "bscc_k_0_leq3" = s=3 & i=0 & r=3 | s=4 & i=0 & r=2 | s=5 & i=0 & r=1 ;
+
+// Number of states: 27
+// Number of BSCCs:  2
