aff6b7f5f2c7f79e5d70e341b60860b5db594ae8
huypn12 <huy.phung1292@gmail.com>
2021-04-22 16:54:28 +0200

    Update run config

--- scripts/config.py
+++ scripts/config.py
@@ -4,11 +4,13 @@ from enum import Enum
 class ConfigKey(Enum):
     is_bee_model = "is_bee_model"
     per_bscc_sampling = "per_bscc_sampling"
+    abc_threshold_decreasing_factor = "abc_threshold_decreasing_factor"
 
 
 _config_dict = {
     ConfigKey.is_bee_model.value: False,
     ConfigKey.per_bscc_sampling.value: 1000,
+    ConfigKey.abc_threshold_decreasing_factor: 1,
 }
 
 
@@ -18,6 +20,10 @@ def is_bee_model():
 
 def per_bscc_sampling():
     return _config_dict[ConfigKey.per_bscc_sampling.value]
+
+
+def abc_threshold_decreasing_factor():
+    return _config_dict[ConfigKey.abc_threshold_decreasing_factor.value]
 
 
 def set_bee_model():
@@ -34,3 +40,11 @@ def set_per_bscc_sampling(n: int):
 
 def unset_per_bscc_sampling():
     _config_dict[ConfigKey.per_bscc_sampling.value] = 1000
+
+
+def set_abc_threshold_decreasing_factor(factor: float):
+    _config_dict[ConfigKey.abc_threshold_decreasing_factor.value] = factor
+
+
+def unset_abc_threshold_decreasing_factor():
+    _config_dict[ConfigKey.abc_threshold_decreasing_factor.value] = 1
